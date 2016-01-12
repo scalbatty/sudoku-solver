2ac6a4c3b2b6dee5800ce626a65468a561150580
Pascal Batty <pascal.batty@soat.fr>
2016-01-12 19:50:46 +0100

    Initial Commit

---
+++ Sudoku/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Sudoku
+//
+//  Created by Pascal Batty on 12/01/2016.
+//  Copyright © 2016 Scalbatty. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
